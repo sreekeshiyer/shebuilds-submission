896c44473d702c82537056978a5a8cc0140d9452
sreekeshiyer <sreekesh.subramanian@gmail.com>
2023-01-08 16:43:22 +0530

    Updated backend middleware

--- forum-backend/config/middlewares.js
+++ forum-backend/config/middlewares.js
@@ -1,12 +1,23 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      header: "*",
+      origin: [
+        "http://localhost:3000",
+        "https://women-in-tech-forum.vercel.app",
+      ],
+    },
+  },
 ];
